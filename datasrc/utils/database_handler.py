1413a2da880c197682aa4a999f1fe490c70cbaf9
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-12-14 01:24:31 +0530

    Handler Code added for MOngo DB Client and AWS S3

---
+++ datasrc/utils/database_handler.py
@@ -0,0 +1,15 @@
+import pymongo
+import os
+
+
+class MongodbClient:
+    client = None
+
+    def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None:
+        if MongodbClient.client is None:
+            MongodbClient.client = pymongo.MongoClient(
+                f"mongodb+srv://{os.environ['MONGO_CLUSTER_USERNAME']}:{os.environ['MONGO_CLUSTER_PASSWORD']}@projects.ch4mixt.mongodb.net/?retryWrites=true&w=majority"
+            )
+        self.client = MongodbClient.client
+        self.database = self.client[database_name]
+        self.database_name = database_name
