81c6bd296938484b98622e9380358d65b47b4062
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-12-14 15:08:10 +0530

    Added components to s3 and mongo components

---
+++ datasrc/components/bulk_upload.py
@@ -0,0 +1,20 @@
+import os
+import base64
+from from_root import from_root
+from tqdm import tqdm
+
+
+def upload_bulk_data(root="images-101"):
+    labels = os.listdir(root)
+    for label in tqdm(labels):
+        data = []
+        images = os.listdir(root + "/" + label)
+        for img in tqdm(images):
+            path = os.path.join(from_root(), root, label, img)
+            with open(rf'{path}', "rb") as img:
+                data.append(base64.b64encode(img.read()).decode())
+
+    print("/nCompleted")
+
+
+upload_bulk_data(root="images-101")
