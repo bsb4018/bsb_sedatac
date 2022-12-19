82107423ca33bfca5219e532adc3a365b2923076
Shibendra Bhattacharjee <storebshiv@gmail.com>
2022-12-19 20:50:53 +0530

    CICD-Workflow_Fix

--- datasrc/utils/database_handler.py
+++ datasrc/utils/database_handler.py
@@ -5,11 +5,12 @@ import os
 class MongodbClient:
     client = None
 
-    def __init__(self, database_name=os.environ["DATABASE_NAME"]) -> None:
+    def __init__(self, database_name=os.environ["MONGO_DATABASE_NAME"]) -> None:
         if MongodbClient.client is None:
             MongodbClient.client = pymongo.MongoClient(
-                f"mongodb+srv://{os.environ['MONGO_CLUSTER_USERNAME']}:{os.environ['MONGO_CLUSTER_PASSWORD']}@projects.ch4mixt.mongodb.net/?retryWrites=true&w=majority"
+                f"mongodb+srv://{os.environ['MONGO_ATLAS_CLUSTER_USERNAME']}:{os.environ['MONGO_ATLAS_CLUSTER_PASSWORD']}@imagesecluster.pemlurn.mongodb.net/?retryWrites=true&w=majority"
             )
         self.client = MongodbClient.client
         self.database = self.client[database_name]
         self.database_name = database_name
+
